cfa7ca847e372b4a13c8906db138a2464cb1710e
Icaro <84683833+The-Icaro@users.noreply.github.com>
2022-06-16 22:54:17 -0300

    Merging FrontEnd (#4)
    * FEATURE: add frontend
    
    * add sidebar to pages
    
    * adjust folder structure
    
    * final styling adjustments
    
    * adjust to 255 max-line-length
    
    * adjust lint checkout
    
    * run python black linter
    
    * removing whitespaces lint
    
    Co-authored-by: Icaro Araújo <icaro.araujo@topi.io>

--- app/service/delivery_api.py
+++ app/service/delivery_api.py
@@ -7,3 +7,15 @@ def get_all_deliveryman():
 
 def get_all_buyers():
     return get("https://cybersecurity-fiap.herokuapp.com/v1/buyer").json()
+
+
+def get_all_deliveries():
+    return get("https://cybersecurity-fiap.herokuapp.com/v1/delivery").json()
+
+
+def find_deliveryman_by_id(id: int):
+    return get(f"https://cybersecurity-fiap.herokuapp.com/v1/deliveryman/{id}").json()
+
+
+def find_buyer_by_id(id: int):
+    return get(f"https://cybersecurity-fiap.herokuapp.com/v1/buyer/{id}").json()
